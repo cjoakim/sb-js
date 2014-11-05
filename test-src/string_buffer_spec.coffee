1567abe83a28eb7fa13a22c9a0fe607f98381f6d
Chris Joakim <christopher.joakim@gmail.comchristopher.joakim@gmail.comchristopher.joakim@gmail.comchristopher.joakim@gmail.com>
2014-11-05 18:25:53 -0500

    Enhanced the spec.

--- test-src/string_buffer_spec.coffee
+++ test-src/string_buffer_spec.coffee
@@ -2,6 +2,18 @@
 
 describe 'StringBuffer', ->
 
-  it "should construct an empty buffer with no argument", ->
+  it "should construct an empty instance with no constructor argument", ->
     sb = new StringBuffer()
     expect(sb.is_empty()).toBe(true)
+    expect(sb.to_string()).toBe('')
+    sb.add('x')
+    expect(sb.is_empty()).toBe(false)
+    expect(sb.to_string()).toBe('x')
+
+
+  it "should construct a populated instance with a constructor String argument", ->
+    sb = new StringBuffer('git')
+    expect(sb.is_empty()).toBe(false)
+    expect(sb.to_string()).toBe('git')
+    sb.add('push')
+    expect(sb.to_string()).toBe('gitpush')
--- test/string_buffer_spec.js
+++ test/string_buffer_spec.js
@@ -1,9 +1,21 @@
 (function() {
   describe('StringBuffer', function() {
-    return it("should construct an empty buffer with no argument", function() {
+    it("should construct an empty instance with no constructor argument", function() {
       var sb;
       sb = new StringBuffer();
-      return expect(sb.is_empty()).toBe(true);
+      expect(sb.is_empty()).toBe(true);
+      expect(sb.to_string()).toBe('');
+      sb.add('x');
+      expect(sb.is_empty()).toBe(false);
+      return expect(sb.to_string()).toBe('x');
+    });
+    return it("should construct a populated instance with a constructor String argument", function() {
+      var sb;
+      sb = new StringBuffer('git');
+      expect(sb.is_empty()).toBe(false);
+      expect(sb.to_string()).toBe('git');
+      sb.add('push');
+      return expect(sb.to_string()).toBe('gitpush');
     });
   });
 
